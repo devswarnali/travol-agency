1287e5f4d651f9ee739fdbb201503ae2542f78f1
devswarnali <166713710+devswarnali@users.noreply.github.com>
2024-10-19 05:51:32 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -135,3 +135,22 @@ $(window).scroll(function() {
   $(".header").removeClass("fixed"); 
   }
 })
+
+// scroll to top
+
+function scrollTop() {
+  if ($(window).scrollTop() > 200) {
+    $(".scrool-to-top").addClass("active");
+  } else {
+    $(".scrool-to-top").removeClass("active");
+  }
+}
+$(function () {
+  scrollTop();
+  $(window).on("scroll", scrollTop);
+
+  $(".scroll-to-top").click(function () {
+    $("html, body").animate({ scrollTop: 0 }, 1);
+    return false;
+  });
+});
